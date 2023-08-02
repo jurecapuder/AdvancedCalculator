540007b089ee8be7fb3b43978e4a22c9fff9647a
jurecapuder <capuder.j@gmail.com>
2023-08-02 22:23:37 +0200

    Updating files

--- script.js
+++ script.js
@@ -1,86 +1,47 @@
-let output = document.getElementById('output');
-let expression = '';
-
-function insertChar(char) {
-    // Check if the last character is a number
-    const lastChar = expression[expression.length - 1];
-    const isLastCharNumber = !isNaN(lastChar);
-
-    // Append the new character accordingly
-    if (isLastCharNumber && !isNaN(char)) {
-        // If the last character is a number and the new character is also a number,
-        // concatenate the new number to the previous one
-        expression += char;
-    } else {
-        // Otherwise, simply append the new character
-        expression += char;
-    }
-
-    updateOutput();
-}
-
-function insertFunction(func) {
-    expression += func + '(';
-    updateOutput();
-}
-
-function clearOutput() {
-    expression = '';
-    updateOutput();
-}
-
-function deleteChar() {
-    expression = expression.slice(0, -1);
-    updateOutput();
-}
-
-function updateOutput() {
-    output.textContent = expression;
+function backspace() {
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
 }
 
 function calculate() {
+    let display = document.getElementById("display");
+    let expression = display.value;
+    let result;
+
     try {
-        const result = eval(expression);
-        expression = result.toString();
+        // Convert trigonometric function inputs from degrees to radians
+        expression = expression.replace(/sin\(/g, 'sin(' + Math.PI / 180 + '*');
+        expression = expression.replace(/cos\(/g, 'cos(' + Math.PI / 180 + '*');
+        expression = expression.replace(/tan\(/g, 'tan(' + Math.PI / 180 + '*');
+
+        result = math.evaluate(expression);
+        display.value = result;
     } catch (error) {
-        expression = 'Error';
-    }
-    updateOutput();
-}
-
-function handleKeyPress(event) {
-    const keyPressed = event.key;
-    if (
-        /[0-9+\-*/().]/.test(keyPressed) || // Allow digits, basic operators, and parentheses
-        (keyPressed === '=' || keyPressed === 'Enter') // Allow Equals or Enter for calculation
-    ) {
-        if (keyPressed === '=' || keyPressed === 'Enter') {
-            calculate();
-        } else {
-            insertChar(keyPressed);
-        }
-    } else if (keyPressed === 'Backspace') {
-        deleteChar();
-    } else if (keyPressed === 'Escape') {
-        clearOutput();
+        display.value = "Error";
     }
 }
 
-// Listen for keypress events on the document
-document.addEventListener('keydown', handleKeyPress);
+function squareRoot() {
+    let display = document.getElementById("display");
+    display.value += "sqrt(";
+}
 
-// Update the expression when the user clicks a button
-document.querySelectorAll('.btn').forEach((button) => {
-    button.addEventListener('click', () => {
-        const char = button.textContent.trim();
-        if (char === '=') {
-            calculate();
-        } else if (char === 'C') {
-            clearOutput();
-        } else if (char === 'DEL') {
-            deleteChar();
-        } else {
-            insertChar(char);
-        }
-    });
-});
+function base10Log() {
+    let display = document.getElementById("display");
+    display.value += "log(";
+}
+
+function pi() {
+    let display = document.getElementById("display");
+    display.value += "pi";
+}
+
+function e() {
+    let display = document.getElementById("display");
+    display.value += "e";
+}
+
+function power() {
+    let display = document.getElementById("display");
+    display.value += "^(";
+}
